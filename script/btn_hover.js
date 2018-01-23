e54099cf6ad5605797ae9e01f14b35283f3bbdf5
GennadyP <PrikhodkoG@gmail.com>
2018-01-23 21:10:37 +0200

    Initial commit

---
+++ script/btn_hover.js
@@ -0,0 +1,45 @@
+var slideBtnSize = document.getElementById('select-btn__size');
+var slideListSize = document.getElementById('slide-list__size');
+slideBtnSize.onmouseover = function (ev) {
+    slideListSize.style.display = 'block';
+    slideBtnSize.style.background = 'orange';
+    slideBtnSize.style.color = '#202835';
+}
+slideBtnSize.onmouseout = function (ev) {
+    slideListSize.style.display = 'none';
+    slideBtnSize.style.background = 'none';
+    slideBtnSize.style.color = '#9da1a6';
+}
+slideListSize.onmouseover = function (ev) {
+    slideListSize.style.display = 'block';
+    slideBtnSize.style.background = 'orange';
+    slideBtnSize.style.color = '#202835';
+}
+slideListSize.onmouseout = function (ev) {
+    slideListSize.style.display = 'none';
+    slideBtnSize.style.background = 'none';
+    slideBtnSize.style.color = '#9da1a6';
+}
+
+var slideBtnQty = document.getElementById('select-btn__qty');
+var slideListQty = document.getElementById('slide-list__qty');
+slideBtnQty.onmouseover = function (ev) {
+    slideListQty.style.display = 'block';
+    slideBtnQty.style.background = 'orange';
+    slideBtnQty.style.color = '#202835';
+}
+slideBtnQty.onmouseout = function (ev) {
+    slideListQty.style.display = 'none';
+    slideBtnQty.style.background = 'none';
+    slideBtnQty.style.color = '#9da1a6';
+}
+slideListQty.onmouseover = function (ev) {
+    slideListQty.style.display = 'block';
+    slideBtnQty.style.background = 'orange';
+    slideBtnQty.style.color = '#202835';
+}
+slideListQty.onmouseout = function (ev) {
+    slideListQty.style.display = 'none';
+    slideBtnQty.style.background = 'none';
+    slideBtnQty.style.color = '#9da1a6';
+}
